66c1833ca1c3d338dd0188c43d96aab948cc0a1c
Raquel Rodriguez Diaz <127392380+Raqu1@users.noreply.github.com>
2025-05-09 09:55:13 +0100

    Add files via upload

---
+++ Ejemplo_01_LeerFichero/MuestraFichero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+// Para StreamReader 
+public class MuestraFichero {
+    private const string NFICH = "texto.txt"; 
+    public static void Main() {
+        StreamReader? sr;
+        string? nFich;
+        string? linea;
+
+        sr = new StreamReader(new FileStream(NFICH,FileMode.Open)); 
+        linea = sr.ReadLine();
+        while(linea != null) {
+            Console.WriteLine(linea);
+            linea = sr.ReadLine();
+        }
+        sr.Close();
+        Console.WriteLine("************************************");
+    }
+}
---
+++ Ejemplo_04_CodificacionesDeCaracteres/Codificaciones.cs
@@ -0,0 +1,42 @@
+﻿// Referencia:
+// https://es.stackoverflow.com/questions/608211/problema-de-codificaci%C3%B3n-en-aplicaci%C3%B3n-de-consola-de-c-no-se-muestran-caracte
+
+using System.Text;
+
+public class Codificaciones
+{
+    public static void Main()
+    {
+        EncodingDeConsola();
+        //ConversionDeEncoding();
+    }
+    private static void EncodingDeConsola(){
+        //Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.OutputEncoding = System.Text.Encoding.ASCII;
+        //Console.InputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.ASCII;
+
+        Console.WriteLine("¿Cuál es tu nombre?");
+        string nombre = Console.ReadLine();
+        //if(nombre == "ñ")
+            Console.WriteLine($"Hola {nombre}!");
+        Console.InputEncoding = System.Text.Encoding.Default;
+        Console.OutputEncoding = System.Text.Encoding.Default;
+    }
+
+
+
+
+
+
+
+    private static void ConversionDeEncoding(){
+        Console.WriteLine("¿Cuál es tu nombre?");
+        string nombre = Console.ReadLine();
+
+        byte[] bytes = Encoding.Default.GetBytes(nombre);
+        nombre = Encoding.UTF8.GetString(bytes);
+
+        Console.WriteLine($"Hola {nombre}!");
+    }
+}
